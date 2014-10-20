7532737bf5962a525be24216899afa2c64c05397
Lioba <ldonoso@dokify.net>
2014-10-20 18:07:00 +0200

    add vies settings

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-var express = require('express');
-var app = express();
+var express	=require('express');
+var app 	= express();
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  	res.render('hello.twig', {
+  		name: 'perry'
+  	});
+});
+
+app.get('/:owner/:repo', function (req, res){
+	var owner 	= req.params.owner;
+	var repo 	= req.params.repo;
+
+	res.render('project.twig', {
+		project	: owner + '/' + repo,
+		owner 	: owner,
+		repo	: repo
+	});
 });
 
 app.get('/settings', function (req, res){
-	res.send('Estas en configuración!')
+	res.render('settings.twig');
 });
+
+
 
 var server = app.listen(8080, function () {
 
