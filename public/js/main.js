cdd19c88c9d89ccf22a861015ec4f20b6e88c27e
Jose Ignacio <joserobleda@gmail.com>
2014-11-17 18:06:14 +0100

    asyn load

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,19 @@
 
+
+var deferreds = [];
 $("div[src]").each(function () {
 	var dom = this,
 	url = $(dom).attr('src');
 
-	$.get(url, function (html) {
-		$(dom).html(html);
+	var deferred = $.get(url);
+	deferred.promise.dom = dom;
+	deferreds.push(deferred);
+});
+
+$.when.apply($, deferreds).then(function () {
+	$.each(arguments, function (i, response) {
+
+		var promise = response[2].promise;
+		$(promise.dom).html(response[0]);
 	});
 });
